2f27518efc224aaa96b2f339893812f197f79610
kidjopa <youtydesteven@gmail.com>
2025-10-27 16:51:34 +0300

    Добавлена проверка на корректность вводимого номера карты (случай, когда введены не цифры)

--- test/test_luhn.py
+++ test/test_luhn.py
@@ -1,5 +1,5 @@
 from src.luhn import luhnСheck
-
+import pytest
 
 def test_good():
     assert luhnСheck("8571 2612 1234 5467")
@@ -7,3 +7,7 @@ def test_good():
 
 def test_bad():
     assert not luhnСheck("4561 2612 1234 5463")
+
+def test_abc():
+    with pytest.raises(ValueError):
+        luhnСheck("abc")
